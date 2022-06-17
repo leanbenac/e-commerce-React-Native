770d763c97aa6ec7dddd555a0596a43add1f0bd6
leanbenac <benacleandro@gmail.com>
2022-06-17 15:15:32 -0300

    challenge location I

--- Screens/SetLocationScreen.js
+++ Screens/SetLocationScreen.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text } from 'react-native'
 import React, { useEffect, useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
